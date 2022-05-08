ceca8eaa950a8c00e4fccf5e17e902cc6960a96d
XuRobotics <liuxu@seas.upenn.edu>
2022-05-08 00:45:41 -0400

    create yaml and sh scripts to prepare for training with Ian pennovation data

---
+++ train/tasks/semantic/train-script-pennovation.sh
@@ -0,0 +1,2 @@
+# --dataset_root_directory should be where the parent folder of sequences folder
+python3 ./train.py --dataset_root_directory ../../../pennovation_data/ -ac ./config/arch/darknet53.yaml -dc ./config/labels/pennovation.yaml -l ../../../log/ -p ../../../pennovation-darknet53/
