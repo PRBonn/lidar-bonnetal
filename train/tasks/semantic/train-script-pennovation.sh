ab46560d6d099207343f38a91f48fb523369bfca
XuRobotics <liuxu@seas.upenn.edu>
2022-05-09 00:44:31 -0400

    remove wrongly added backslash symbol in sh files

--- train/tasks/semantic/train-script-pennovation.sh
+++ train/tasks/semantic/train-script-pennovation.sh
@@ -1,2 +1,2 @@
 # --dataset_root_directory should be where the parent folder of sequences folder
-python3 ./train.py --dataset_root_directory ../../../pennovation_dataset/ -ac ./config/arch/darknet53.yaml -dc ./config/labels/pennovation.yaml -l ../../../log/ -p ../../../pennovation-darknet53/
+python3 ./train.py --dataset_root_directory ../../../pennovation_dataset/ -ac ./config/arch/darknet53.yaml -dc ./config/labels/pennovation.yaml -l ../../../log/ -p ../../../pennovation-darknet53
