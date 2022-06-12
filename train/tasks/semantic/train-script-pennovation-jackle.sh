7005acb2863e70dc9b9526dc1db2ce4645d7a583
XuRobotics <liuxu@seas.upenn.edu>
2022-06-11 22:56:28 -0400

    update train sh file for Ian

--- train/tasks/semantic/train-script-pennovation-jackle.sh
+++ train/tasks/semantic/train-script-pennovation-jackle.sh
@@ -1,2 +1,2 @@
 # --dataset_root_directory should be where the parent folder of sequences folder
-python3 ./train.py --dataset_root_directory ../../../pennovation_dataset_jackle/ -ac ./config/arch/darknet53-1024px-pennovation-jackle.yaml -dc ./config/labels/pennovation-jackle.yaml -l ../../../logs-jackle/ -p ../../../pennovation-darknet53-jackle
+python3 ./train.py --dataset_root_directory ../../../pennovation_dataset_jackle/ -ac ./config/arch/darknet-smallest-1024px-pennovation-jackle.yaml -dc ./config/labels/pennovation-jackle.yaml -l ../../../logs-jackle/ -p ../../../pennovation-darknet-smallest-jackle
